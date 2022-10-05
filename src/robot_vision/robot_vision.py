f633d39fb13008bb131c1233699fc91f0b168c78
DukMastaaa <64053792+DukMastaaa@users.noreply.github.com>
2022-10-05 16:17:01 +1000

    push work to main

--- src/robot_vision/robot_vision.py
+++ src/robot_vision/robot_vision.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 import rospy
-from sensor_msgs.msg import Pose
-from fiducial_msgs.msg import FiducialTransform 
+from geometry_msgs.msg import Pose,Point
+from fiducial_msgs.msg import FiducialTransformArray
+
 import tf2_ros
 from tf_conversions import transformations as tfct
 import modern_robotics as mr
 import numpy as np
 
 # transformation matrix from the base frame to the camera frame
-T_bc = np.array(
+T_bc = np.array([
     [0, 1, 0, 0.16],
     [1, 0, 0, 0],
-    [0, 0, -1, 0.445]
+    [0, 0, -1, 0.445],
+    [0,0,0,1]]
 )
 
 class RobotVision:
@@ -26,27 +28,33 @@ class RobotVision:
         )
         self.sub = rospy.Subscriber(
             self.NODE_NAME_TO_SUBSCRIBE,
-            FiducialTransform,
+            FiducialTransformArray,
             self.callback
         )
 
-    def callback(self, fiducialtransform: FiducialTransform) -> None:
-        transform = fiducialtransform.transform
-        t_ct = transform.translation
-        q_ct = transform.rotation
 
-        # from week 5 prac
-        p_ct = np.array([t_ct.x, t_ct.y, t_ct.z])
-        R_ct = tfct.quaternion_matrix(np.array([q_ct.x, q_ct.y, q_ct.z, q_ct.w]))[:3, :3]
-        T_ct = mr.RpToTrans(R_ct, p_ct)
+    def callback(self, fiducialtransform: FiducialTransformArray) -> None:
+        fidtransform = fiducialtransform.transforms
+        if len(fidtransform) >= 1:
+            t_form = fidtransform[0].transform
+            t_ct = t_form.translation
+            q_ct = t_form.rotation
 
-        T_bt = T_bc @ T_ct
+            # from week 5 prac
+            p_ct = np.array([t_ct.x, t_ct.y, t_ct.z])
+            R_ct = tfct.quaternion_matrix(np.array([q_ct.x, q_ct.y, q_ct.z, q_ct.w]))[:3, :3]
+            T_ct = mr.RpToTrans(R_ct, p_ct)
 
-        msg = Pose(
-            
-        )
+            T_bt = T_bc @ T_ct
 
-        self.pub.publish(msg)
+            R_bt, p_bt = mr.TransToRp(T_bt)
+            #q_bt = tfct.quaternion_from_matrix(R_bt)
+
+            msg = Pose(
+                position = Point(p_bt[0], p_bt[1], p_bt[2])
+            )
+
+            self.pub.publish(msg)
 
 def main():
     rospy.init_node("tag_detection", anonymous = True)
