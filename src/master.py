cc89fafbccf4f1e6eefcf9fefa76a7449e0ac1e5
miabroedelet <76677643+miabroedelet@users.noreply.github.com>
2022-10-19 12:59:34 +1000

    Update master.py

--- src/master.py
+++ src/master.py
@@ -9,3 +9,10 @@ from collections import defaultdict
 import constants
 
 class Master:
+  """
+    Controls sequencing of planner node depending on the level selected.
+    Level 1 - one block, rotating conveyor
+    Level 2 - multiple blocks, rotating conveyor
+    Level 3a - multiple blocks, stationary conveyor
+    Level 3b - multiple blocks, rotating conveyor, pickup whilst in motion
+    """
