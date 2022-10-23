f0c2af9b2901530eddbc8ce52396330d5be1d862
spickers <112992855+spickers@users.noreply.github.com>
2022-10-24 09:49:25 +1000

    Final copy of inverse_kinematics.py

--- src/robot_control/inverse_kinematics.py
+++ src/robot_control/inverse_kinematics.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 
+# 0 Packages and global variables
+# 0.1 Importing external packages
 import rospy
 import modern_robotics as mr
-
 from std_msgs.msg import Header, Bool
 from sensor_msgs.msg import JointState
 from geometry_msgs.msg import Pose
-
 import numpy as np
 from numpy import cos, sin, sqrt, pi
 from numpy import arctan2 as atan2
-
 import constants
-
 from typing import Tuple
-
 from robot_msgs.srv import DoInverseKinematics, DoInverseKinematicsResponse
 
+# 0.2 Setting the home configuration
 Slist = np.array([
     [0, 0, 1, 0, 0, 0],
     [0, 1, 0, -constants.L1, 0, 0],
@@ -25,28 +23,35 @@ Slist = np.array([
 ]).T
 M = mr.RpToTrans(np.eye(3), np.array([0, 0, constants.L1 + constants.L2 + constants.L3 + constants.L4]))
 
+# 1 Inverse Kinematics
 class InverseKinematics:
+    
+    # 1.1 Setting the node names and solution type
     NODE_NAME_TO_PUBLISH = "desired_joint_states"
     NODE_NAME_TO_SUBSCRIBE = "desired_pose"
+    USE_NUMERICAL = False # USE_NUMERICAL is a toggle variable for testing our inverse kinematics work against the modern robotics method
     
-    USE_NUMERICAL = False
-    
+    # 1.2 Initialising the class
     def __init__(self):
+        
+        # 1.2.1 Publishing the joint state solution of the inverse kinematics process
         self.pub = rospy.Publisher(
             self.NODE_NAME_TO_PUBLISH,
             JointState,
             queue_size=10
         )
+        
+        # 1.2.2 Providing a service for the planner node
         self.srv = rospy.Service(
             'do_inverse_kinematics',  # service name, MUST be consistent with planner
             DoInverseKinematics,  # class
             self.callback
         )
         
-        # use previously calculated joint angles
-        # as initial guess for numerical IK
+        # 1.2.3 Setting initial joint state guess (Numerical IK only)
         self.prev_joint_angles = np.zeros(4)
     
+    # 1.3 Methods for inverse kinematics solution
     @staticmethod
     def pose_will_collide(pose: Pose, theta_1, theta_2, theta_3, theta_4) -> bool:
         """
@@ -54,15 +59,13 @@ class InverseKinematics:
         either with the environment or with the robot itself.
         """
         x, y, z = pose.position.x, pose.position.y, pose.position.z
-        # ground
+        
         if z < constants.HEIGHT_ABOVE_GROUND:
-            return True
-        # wall
+            return True # instance of a ground collision
+        
         if x < constants.WALL_X_LIMIT:
-            return True
-        # self-collision
-        # from the hint given in the milestone, we can implement soft joint limits
-        # ...seems unreliable, will think about it
+            return True # instance of a wall collision
+
         return False
     
     @staticmethod
@@ -85,6 +88,7 @@ class InverseKinematics:
         defined in constants.py, desired position (px, py, pz),
         and desired end-effector angle theta_e measured down from horizontal.
         """
+        
         # We want to reduce the 4R arm to a 2R arm in (r, z) coordinates, where
         # the start of the 2R arm is L1 above the first joint, and 
         # the end is the position of joint 4 relative to the start.
@@ -106,8 +110,13 @@ class InverseKinematics:
         return theta_1, theta_2, theta_3, theta_4
 
     def numerical(self, px, py, pz):
+        """
+        Utilises the modern robotics library to retrieve the numerical IK solution for our robot
+        with lengths defined in constants.py and the desired position (px, py, pz).
+        """
+        
         eomg = float('inf')  # large tolerance so orientation doesn't matter
-        ev = 0.1
+        ev = 0.1 # a threshold for the numerical IK solution
         T = mr.RpToTrans(np.eye(3), [px, py, pz])
         soln, success = mr.IKinSpace(Slist, M, T, self.prev_joint_angles, eomg, ev)
         soln = (soln + np.pi) % (2 * np.pi) - np.pi
@@ -123,10 +132,12 @@ class InverseKinematics:
         This is necessary as the way we assembled the motors isn't
         consistent with the definitions of theta for IK.
         """
+        
         # compensate for motor orientation
         # theta_2 = np.deg2rad(90) - theta_2
         theta_2 = -theta_2
         theta_3 = -theta_3
+        
         # compensate for different motor IDs
         # angle_i is the angle that the motor with ID i should recieve
         angle_1 = theta_4
@@ -142,10 +153,9 @@ class InverseKinematics:
         to the next stage.
         Returns whether IK succeded as the response.
         """
-        # split up request
+        # Split-up request
         pose = req.pose
         theta_e = req.theta_e
-        
         try:
             if self.USE_NUMERICAL:
                 theta_1, theta_2, theta_3, theta_4 = self.numerical(
@@ -163,10 +173,8 @@ class InverseKinematics:
         angle_1, angle_2, angle_3, angle_4 = self.compensated_angles(
             theta_1, theta_2, theta_3, theta_4
         )
-
-        # if self.pose_will_collide(pose, theta_1, theta_2, theta_3, theta_4):
-        #     raise ValueError("collision")
         
+        # Message containing the joint state solution
         msg = JointState(
             # Set header with current time
             header=Header(stamp=rospy.Time.now()),
@@ -174,11 +182,11 @@ class InverseKinematics:
             name=['joint_1', 'joint_2', 'joint_3', 'joint_4'],
             position=[angle_1, angle_2, angle_3, angle_4]
         )
-        
         self.pub.publish(msg)
         
         return DoInverseKinematicsResponse(Bool(data=True))
 
+# 2 Main function
 def main():
     # Initialise node with any node name
     rospy.init_node('inverse_kinematics', anonymous = True)
