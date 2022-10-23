0a272bac2c17af7f9b80dbb07ad4de4b8e08f0c7
spickers <112992855+spickers@users.noreply.github.com>
2022-10-24 09:18:53 +1000

    Final copy of constants.py

--- src/robot_control/constants.py
+++ src/robot_control/constants.py
@@ -1,10 +1,8 @@
-L1 = 0.053
-L2 = 0.117
-L3 = 0.095
-L4 = 0.09
+L1 = 0.053 # the length of the first link in metres
+L2 = 0.117 # the length of the second link in metres
+L3 = 0.095 # the length of the third link in metres
+L4 = 0.09 # the length of the fourth link in metres
 
-# This is measured down from *horizontal*, in degrees
-THETA_E_DEG = 70
-
-HEIGHT_ABOVE_GROUND = -0.025
-WALL_X_LIMIT = 0.057
+THETA_E_DEG = 70 # the effective angle, measured down from the *horizontal*, in degrees
+HEIGHT_ABOVE_GROUND = -0.025 # the height above the ground, as defined by the turn-table
+WALL_X_LIMIT = 0.057 # the limit of the robot's range, as defined in the x-axis
