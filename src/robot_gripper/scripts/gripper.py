017ee0c90c0b2ce699cb3344ee4a4287e5a3f28e
spickers <112992855+spickers@users.noreply.github.com>
2022-10-24 09:56:04 +1000

    Final copy of robot_gripper.py

--- src/robot_gripper/scripts/gripper.py
+++ src/robot_gripper/scripts/gripper.py
@@ -5,14 +5,20 @@ from std_msgs.msg import Bool
 
 
 class RobotGripper():
+    
+    # 1 Node names
     NODE_NAME_TO_SUBSCRIBE = "desired_gripper_pos"
     NODE_NAME_TO_PUBLISH = "gripper_pos"
     
-    CLOSE = 1850
-    OPEN = 2400
-    PWM_PIN = 18
-
+    # 2 Servo motor parameters
+    CLOSE = 1850 # pulse width for closed state
+    OPEN = 2400 # pulse width for open state
+    PWM_PIN = 18 # the pin designated for communicating with the gripper servo motor
+    
+    # 3 Class initialisation
     def __init__(self):
+        
+        # 3.1 Setting the publishers and subscribers
         self.pub = rospy.Publisher(
             self.NODE_NAME_TO_PUBLISH,
             Bool,
@@ -23,11 +29,17 @@ class RobotGripper():
             Bool,
             self.callback
         )
-
+        
+        # 3.2 Setting an object of pigpio.pi() type
         self.rpi = pigpio.pi()
         self.rpi.set_mode(self.PWM_PIN, pigpio.OUTPUT)
-
+        
+    # 4 Callback
     def callback(self, close: Bool):
+        """
+        Based on the close input, the servo motor controlling the gripper sets it to an closed (True) or open (False) state 
+        """
+        
         if close.data: 
             self.rpi.set_servo_pulsewidth(self.PWM_PIN, self.CLOSE)
             print('should be closed')
